412da80c0121fbbc5a89315f6fe72599d589e63d
Salil Panikkaveettil <salilpa007@gmail.com>
2013-09-25 11:13:17 +0530

    Creating function for the string transformation from unicode to normal string

--- amazonapi.py
+++ amazonapi.py
@@ -1,6 +1,13 @@
 from bs4 import BeautifulSoup
 import requests
 import re
+
+def string_transform(value, replace_text=None):
+    value = value.text.replace(u'\xa0',u' ') #replacing the non-breaking space in Latin1 (ISO 8859-1) with our own space
+    if replace_text:
+        value = value.replace(replace_text.text,'')
+    return str(re.sub('[|:#]','',value).strip()) #replacing some special characters and stripping it
+
 class AMAZONAPI:
 
     url_amazon = "http://www.amazon.in/gp/product/"#this will be the base url for all the product. ISBN10 number will be appended to this
@@ -37,21 +44,21 @@ class AMAZONAPI:
         product_description = soup.find(id='productDescription')
         product_details = soup.find('td', class_='bucket') #all isbn values and other data are stored here
         if title:
-            self.response_json["title"] = title.text.strip()
+            self.response_json["title"] = string_transform(title)
         if list_price:
-            self.response_json["list price"] = list_price.text.strip()
+            self.response_json["list price"] = string_transform(list_price)
         if actual_price:
-            self.response_json["actual price"] = actual_price.text.strip()
+            self.response_json["actual price"] = string_transform(actual_price)
         if book_description:
             book_desc_json = {}
             span_content = book_description.find_all('span', attrs={'class':None})#this contains all the content
             span_heading = book_description.find_all('span', class_='byLinePipe')#this contains all the title
             for title, content in zip(span_heading, span_content):
                 if title.text != '' and content.text != '':#assigning key values to each
-                    book_desc_json[re.sub('[|:]','',title.text).strip()] = re.sub('[|:]','',content.text).strip()
+                    book_desc_json[string_transform(title)] = string_transform(content)
             book_details = soup.find(id='postBodyPS')
             if book_details:
-                book_desc_json['book details'] = book_details.text.strip()
+                book_desc_json['book details'] = string_transform(book_details)
             self.response_json["book description"] = book_desc_json
 
         if product_description:
@@ -60,7 +67,7 @@ class AMAZONAPI:
             product_desc_contents = product_description.find_all('div', class_='productDescriptionWrapper')
             for title, content in zip(product_desc_titles, product_desc_contents):#iterating over both objects at once
                 if title.text != '' and content.text != '':#assigning key values to each
-                    product_desc_json[re.sub('[|:]','',title.text).strip()] = re.sub('[|:]','',content.text).strip()
+                    product_desc_json[string_transform(title)] = string_transform(content)
             self.response_json["product description"] = product_desc_json
 
         if product_details:
@@ -72,22 +79,22 @@ class AMAZONAPI:
                     if title.text.strip() == 'Average Customer Review:':
                         rating = li.find('span', class_ = 'swSprite')
                         if rating:
-                            product_details[re.sub('[|:]','',title.text).strip()] = re.sub('[|:]','',rating.text).strip()
+                            product_details[string_transform(title)] = string_transform(rating)
                     elif title.text.strip() == 'Amazon Bestsellers Rank:':
                         amazon_bestsellers = {}
                         m = re.search('([\d,]*\d+) in Books', li.text)
                         if m:
                             rank =  m.groups()[0]
-                            amazon_bestsellers['Books'] = rank
+                            amazon_bestsellers['Books'] = str(rank)
                         ranks = li.find_all('li', class_='zg_hrsr_item')
                         for item in ranks:
                             category = item.find('span', class_='zg_hrsr_ladder')
                             rank = item.find('span', class_='zg_hrsr_rank')
                             if rank and category:
-                                amazon_bestsellers[re.sub('[|:]','',category.text).strip()] = re.sub('[|:]','',rank.text).strip()
+                                amazon_bestsellers[string_transform(category)] = string_transform(rank)
                         product_details['Amazon Bestsellers'] = amazon_bestsellers
                     else:
-                        product_details[re.sub('[|:]','',title.text).strip()] = re.sub('[|:]','',li.text.replace(title.text,'')).strip()
+                        product_details[string_transform(title)] = string_transform(li,title)
             self.response_json["product details"] = product_details
 
     def get_json(self):
